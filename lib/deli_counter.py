51a6eab4d096be1f13e8034634bd79958e8e3d40
Bailey Cage <bailey.cage@gmail.com>
2023-02-13 12:34:43 -0500

    implement now_serving() function

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -11,6 +11,11 @@ def take_a_number(katz_deli, name):
     katz_deli.append(name)
     print(f"Welcome, {name}. You are number {len(katz_deli)} in line.")
 
-def now_serving():
-    pass
+def now_serving(katz_deli):
+    if len(katz_deli) == 0:
+        print("There is nobody waiting to be served!")
+    else:
+        serving = katz_deli[0]
+        del katz_deli[0]
+        print(f"Currently serving {serving}.")
 
